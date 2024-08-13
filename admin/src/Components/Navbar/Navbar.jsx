a0f8cdd91626daf2b70890a2fb92e715964a7aef
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-08-13 18:18:31 -0500

    new icon in admin

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import navlogo from '../../assets/nostalgiafy_with_tagline.png'
 import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
