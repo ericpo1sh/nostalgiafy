fba79277d65af2289e29d753d5c80b7888802dc4
ericpo1sh <ericpo1sh@gmail.com>
2024-08-13 10:50:03 -0500

    test

--- backend/index.js
+++ backend/index.js
@@ -165,8 +165,9 @@ app.get('/paginatedItems', async (req, res) => {
 // route for adding items into the cart and storing it in the users database
 
 app.post('/addtocart', fetchUser, async (req, res) => {
+  console.log(userData.cartData[req.body.itemId])
   let userData = await Users.findOne({_id:req.user.id});
-  userData.cartData[req.body.item.id] + 1;
+  userData.cartData[req.body.itemId] + 1;
   await Users.findOneAndUpdate({_id:req.user.id}, {cartData: userData.cartData});
   res.send('Added to cart')
 })
@@ -174,9 +175,10 @@ app.post('/addtocart', fetchUser, async (req, res) => {
 // route for removing items from cart and updating user database
 
 app.post('/removefromcart', fetchUser, async (req, res) => {
+  console.log(userData.cartData[req.body.itemId])
   let userData = await Users.findOne({_id:req.user.id});
-  if (userData.cartData[req.body.item.id] > 0) {
-    userData.cartData[req.body.item.id] -= 1;
+  if (userData.cartData[req.body.itemId] > 0) {
+    userData.cartData[req.body.itemId] -= 1;
   }
   await Users.findOneAndUpdate({_id:req.user.id}, {cartData: userData.cartData});
   res.send('Removed from cart')
