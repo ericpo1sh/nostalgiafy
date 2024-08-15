7cad7901f8ef335067368e59b8504b904e344f69
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-08-14 21:25:26 -0500

    MAJOR FIX TO ADMIN OMG OMG OMG OMG

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,32 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddProduct.css'
-import upload_area from '../../assets/upload_area.svg'
 
 const AddProduct = () => {
+
+  const [productDetails,setProductDetails] = useState({
+    id:"",
+    name:"",
+    cost:"",
+    img:"",
+    description:"",
+    rating:"",
+    rating_img:"",
+  })
+
+  const changeHandler = (e) =>{
+    setProductDetails({...productDetails,[e.target.name]:e.target.value})
+  }
+
+  const Add_Product = async () => {
+    console.log(productDetails);
+    let product = productDetails;
   
+    await fetch('https://nostalgiafy.onrender.com/addproduct', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        data.success ? alert('Product Added') : alert('Product not added');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('An error occurred while adding the product');
+      });    
+  };
+  
+
   return (
     <div className='add-product'>
       <div className="addproduct-itemfield">
         <p>Product Title</p>
-        <input type="text" name='name' placeholder='Type here' />
+        <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here' />
       </div>
       <div className="addproduct-itemfield">
         <p>Product Description</p>
-        <input type="text" name='description' placeholder='Type here' />
-      </div>
-      <div className="addproduct-price">
-        <div className="addproduct-itemfield">
-          <p>Price</p>
-          <input type="text" name="price" placeholder='Type here' />
-        </div>
+        <input value={productDetails.description} onChange={changeHandler} type="text" name='description' placeholder='Type here' />
       </div>
       <div className="addproduct-itemfield">
-        <label htmlFor='file-input'>
-          <img src={upload_area} className='addproduct-thumbnail-img' alt="" />
-        </label>
-        <input type="file" name='image' id='file-input' hidden/>
+        <p>Cost</p>
+        <input value={productDetails.cost} onChange={changeHandler} type="text" name="cost" placeholder='Type here' />
       </div>
-      <button className='addproduct-btn'>ADD</button>
+      <div className="addproduct-itemfield">
+        <p>Image URL</p>
+        <input value={productDetails.img} onChange={changeHandler} type="text" name='img' placeholder='Paste Image URL here' />
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Rating</p>
+        <input value={productDetails.rating} onChange={changeHandler} type="text" name='rating' placeholder='Enter Rating here' />
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Rating Image</p>
+        <input value={productDetails.rating_img} onChange={changeHandler} type="text" name='rating_img' placeholder='Paste Star Rating Image URL here' />
+      </div>
+      <button onClick={()=>{Add_Product()}} className='addproduct-btn'>ADD</button>
     </div>
   )
 }
